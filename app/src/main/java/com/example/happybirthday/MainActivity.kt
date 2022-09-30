260e6c46cc72a77507467d94b4db7b23645e483c
Vlad <ricean.vlad7@gmail.com>
2022-09-30 09:46:28 +0300

    -finished app (need some refactoring with code duplication - see solution code)
    https://developer.android.com/codelabs/basic-android-kotlin-compose-composables-practice-problems?authuser=1&continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fandroid-basics-compose-unit-1-pathway-3%3Fauthuser%3D1%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-compose-composables-practice-problems#3

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +39,96 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column() {
+        Row() {
+            Column(
+                Modifier
+                    .background(color = Color.Green)
+                    .fillMaxHeight(fraction = 0.5f)
+                    .fillMaxWidth(fraction = 0.5f)
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = stringResource(id = R.string.text_composable),
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp,
+                    modifier = Modifier
+                        .padding(bottom = 16.dp)
+                )
+                Text(
+                    text = stringResource(id = R.string.display_text_and_follows),
+                    textAlign = TextAlign.Justify
+                )
+            }
+            Column(
+                Modifier
+                    .background(color = Color.Yellow)
+                    .fillMaxHeight(0.5f)
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = stringResource(id = R.string.text_composable),
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp,
+                    modifier = Modifier
+                        .padding(bottom = 16.dp)
+                )
+                Text(
+                    text = stringResource(id = R.string.display_text_and_follows),
+                    textAlign = TextAlign.Justify
+                )
+            }
+        }
+        Row() {
+            Column(
+                Modifier
+                    .background(color = Color.Cyan)
+                    .fillMaxHeight()
+                    .fillMaxWidth(fraction = 0.5f)
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = stringResource(id = R.string.text_composable),
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp,
+                    modifier = Modifier
+                        .padding(bottom = 16.dp)
+                )
+                Text(
+                    text = stringResource(id = R.string.display_text_and_follows),
+                    textAlign = TextAlign.Justify
+                )
+            }
+            Column(
+                Modifier
+                    .background(color = Color.LightGray)
+                    .fillMaxHeight()
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = stringResource(id = R.string.text_composable),
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp,
+                    modifier = Modifier
+                        .padding(bottom = 16.dp)
+                )
+                Text(
+                    text = stringResource(id = R.string.display_text_and_follows),
+                    textAlign = TextAlign.Justify
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
